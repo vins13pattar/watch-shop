b1b608bf0a4b605a522c9073cc3bbdd383e00e42
Vinod SP <vinodsp@tharsha.com>
2024-07-12 20:08:53 +0530

    Django heroku configured

--- WatchShop/settings.py
+++ WatchShop/settings.py
@@ -134,3 +134,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+import django_heroku
+django_heroku.settings(locals())
+
